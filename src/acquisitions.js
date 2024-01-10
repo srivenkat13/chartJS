d6c1c1337e052f68ba560d0f904554a118162ab4
srivenkat13 <srivenkatsairaj13@gmail.com>
2024-01-10 17:02:11 +0530

    chart with API data

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -2,24 +2,29 @@ import { Chart, plugins } from "chart.js/auto";
 
 import { getAquisitionsByYear } from "./api";
 
+
 (async function () {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 24 },
-    { year: 2017, count: 27 },
-    { year: 2018, count: 38 },
-    { year: 2019, count: 28 },
-    { year: 2020, count: 48 },
-    { year: 2021, count: 18 },
-    { year: 2022, count: 29 },
-    { year: 2023, count: 20 },
-    { year: 2024, count: 30 },
-  ];
+  // const data = [
+  //   { year: 2010, count: 10 },
+  //   { year: 2011, count: 20 },
+  //   { year: 2012, count: 15 },
+  //   { year: 2013, count: 25 },
+  //   { year: 2014, count: 22 },
+  //   { year: 2015, count: 30 },
+  //   { year: 2016, count: 24 },
+  //   { year: 2017, count: 27 },
+  //   { year: 2018, count: 38 },
+  //   { year: 2019, count: 28 },
+  //   { year: 2020, count: 48 },
+  //   { year: 2021, count: 18 },
+  //   { year: 2022, count: 29 },
+  //   { year: 2023, count: 20 },
+  //   { year: 2024, count: 30 },
+  // ];
+  
+  const data = await getAquisitionsByYear()
+
+  
 
   new Chart(document.getElementById("acquisitions"), {
     type: "bar",
@@ -45,4 +50,6 @@ import { getAquisitionsByYear } from "./api";
       ],
     },
   });
+
+  
 })();
